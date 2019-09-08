ef6a01cbd30a8af3216fc43a036ee0fb9e28ee64
asanamandra <asanamandra@gmail.com>
2019-09-08 07:53:28 -0400

    Fixed port issue

--- app.js
+++ app.js
@@ -279,7 +279,7 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
 }
-app.listen(port);
+// app.listen(port);
 
 app.listen(port, function(){
   console.log("server started successfully");
