b15a761f53f21418cfa51e73d4d111bda3976830
asanamandra <asanamandra@gmail.com>
2019-09-08 07:37:27 -0400

    Before going on heroku

--- app.js
+++ app.js
@@ -275,7 +275,12 @@ app.route("/users/:userEmail")
 });
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
 
-app.listen(3000, function(){
-  console.log("started on port 3000");
+app.listen(port, function(){
+  console.log("server started successfully");
 });
