dda08fff9580898e15ba711d870ab71d3e72f7aa
asanamandra <asanamandra@gmail.com>
2019-09-14 05:49:59 -0400

    Maybe fixed db problem

--- app.js
+++ app.js
@@ -35,9 +35,7 @@ app.use(passport.session());
 app.use(flash());
 
 const password = process.env.DB_PASS;
-mongoose.connect("mongodb+srv://admin-allen:" + password + "@cluster0-5gksb.mongodb.net/loginDB", {
-  useNewUrlParser: true
-});
+mongoose.connect("mongodb+srv://admin-allen:" + password + "@cluster0-5gksb.mongodb.net/loginDB");
 // mongoose.connect("mongodb://localhost:27017/loginDB", {useNewUrlParser: true});
 //deprecation warning
 mongoose.set("useCreateIndex", true);
