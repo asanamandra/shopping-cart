d589e11e8d05b1678e4d10eafcae72f5f2022b9e
asanamandra <asanamandra@gmail.com>
2019-09-14 05:44:36 -0400

    Maybe fixed db problem

--- app.js
+++ app.js
@@ -301,8 +301,8 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
 }
-// app.listen(port);
+app.listen(port);
 
-app.listen(port, function() {
-  console.log("server started successfully");
-});
+// app.listen(port, function() {
+//   console.log("server started successfully");
+// });
